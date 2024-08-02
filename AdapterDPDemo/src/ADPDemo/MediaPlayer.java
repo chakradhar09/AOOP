6ae100703e8ff4550d1d7df323ee4793f7a749d5
Chilakapati Srijaya Chakradhar <67419391+chakradhar09@users.noreply.github.com>
2024-08-02 12:55:10 +0530

    Delete AdapterDPDemo directory

--- AdapterDPDemo/src/ADPDemo/MediaPlayer.java
+++
@@ -1,5 +0,0 @@
-package ADPDemo;
-//Step 1 : Create interfaces for Media Player and Advanced Media Player. (MediaPlayer.java & AdvancedMediaPlayer.java)
-public interface MediaPlayer {
-	public void play(String audioType, String fileName);
-}
