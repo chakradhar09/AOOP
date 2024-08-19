455894ef17edae7e1f290a471e15cb63dd4d444b
Chilakapati Srijaya Chakradhar <jaya.chakri2005@gmail.com>
2024-08-19 13:12:16 +0530

    30360_6A

---
+++ 30360_6A/30360_6ALAB2/src/TODO/TODO.java
@@ -0,0 +1,42 @@
+package TODO;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TODO {
+    private final List<String> todo;
+
+    public TODO(){
+        this.todo = new ArrayList<>();
+    }
+
+    public boolean isTask(String task){
+
+        return todo.contains(task);
+    }
+    public String isTask(int position){
+
+        return todo.get(position);
+    }
+
+    public boolean addTask(String task){
+
+        return todo.add(task);
+    }
+    public String updateTask(int position, String task){
+
+        return todo.set(position, task);
+    }
+    public boolean removeTask(String task){
+
+        return todo.remove(task);
+    }
+    public boolean removeTaskAll(){
+
+        return todo.removeAll(todo);
+    }
+    public void listAll(){
+        for(String i: todo)
+            System.out.println(i);
+    }
+}
